ef440bef60c00d4633ad0d97a021f3ea3a3fd96a
wandinhawright <wwanda.wwright@gmail.com>
2025-04-11 21:28:18 -0300

    Correção dos elementos visuais da página index

--- receitas/views.py
+++ receitas/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def indexView(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
 
 def ingredientesView(request):
-    return render(request, 'ingredientes.html')
+    return render(request, "ingredientes.html")
